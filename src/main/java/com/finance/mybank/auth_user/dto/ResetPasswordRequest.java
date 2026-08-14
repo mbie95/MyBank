acb097d45953c52d4a76b388a76dc574bf7cb7f9
Marcin <marcinbielen995@gmail.com>
2026-08-14 11:56:25 +0200

    Create ResetPasswordRequest

---
+++ src/main/java/com/finance/mybank/auth_user/dto/ResetPasswordRequest.java
@@ -0,0 +1,12 @@
+package com.finance.mybank.auth_user.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ResetPasswordRequest {
+    private String email;
+    private String code;
+    private String newPassword;
+}
