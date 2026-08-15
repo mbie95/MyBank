1ec4733cc41b7614b795a86a7dacbd0e41b45ad7
Marcin <marcinbielen995@gmail.com>
2026-08-15 12:48:25 +0200

    Create TransactionDTO

--- src/main/java/com/finance/mybank/account/dto/AccountDTO.java
+++ src/main/java/com/finance/mybank/account/dto/AccountDTO.java
@@ -8,6 +8,7 @@ import com.finance.mybank.auth_user.dto.UserDTO;
 import com.finance.mybank.enums.AccountStatus;
 import com.finance.mybank.enums.AccountType;
 import com.finance.mybank.enums.Currency;
+import com.finance.mybank.transaction.dto.TransactionDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
---
+++ src/main/java/com/finance/mybank/transaction/dto/TransactionDTO.java
@@ -0,0 +1,44 @@
+package com.finance.mybank.transaction.dto;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.finance.mybank.account.dto.AccountDTO;
+import com.finance.mybank.enums.TransactionStatus;
+import com.finance.mybank.enums.TransactionType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransactionDTO {
+
+    private Long id;
+
+    private BigDecimal amount;
+
+    private TransactionType transactionType;
+
+    private LocalDateTime transactionDate;
+
+    private String description;
+
+    private TransactionStatus status;
+
+    @JsonBackReference
+    private AccountDTO account;
+
+    private String sourceAccount;
+
+    private String destinationAccount;
+
+}
