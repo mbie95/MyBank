8554abdee0e6b5947d40f3cee9343a38e2d6c34c
Marcin <marcinbielen995@gmail.com>
2026-08-15 14:52:54 +0200

    Create NotificationDTO

---
+++ src/main/java/com/finance/mybank/notification/dto/NotificationDTO.java
@@ -0,0 +1,40 @@
+package com.finance.mybank.notification.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.finance.mybank.enums.NotificationType;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationDTO {
+
+    private Long id;
+
+    private String subject;
+
+    @NotBlank(message = "Recipient is required")
+    private String recipient;
+
+    private String body;
+
+    private NotificationType type;
+
+    private LocalDateTime createdAt;
+
+    private String templateName;
+
+    private Map<String, Object> templateVariables;
+
+}
