52f249fd67d143ef63eb67a95cd0e6d6381328d1
Marcin <marcinbielen995@gmail.com>
2026-08-23 09:41:33 +0200

    Create UserServiceImpl

---
+++ src/main/java/com/finance/mybank/auth_user/service/impl/UserServiceImpl.java
@@ -0,0 +1,169 @@
+package com.finance.mybank.auth_user.service.impl;
+
+import com.finance.mybank.auth_user.dto.UpdatePasswordRequest;
+import com.finance.mybank.auth_user.dto.UserDTO;
+import com.finance.mybank.auth_user.entity.User;
+import com.finance.mybank.auth_user.repo.UserRepo;
+import com.finance.mybank.auth_user.service.UserService;
+import com.finance.mybank.exception.BadRequestException;
+import com.finance.mybank.exception.NotFoundException;
+import com.finance.mybank.notification.dto.NotificationDTO;
+import com.finance.mybank.notification.service.NotificationService;
+import com.finance.mybank.res.Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepo userRepo;
+    private final NotificationService notificationService;
+    private final PasswordEncoder passwordEncoder;
+    private final ModelMapper modelMapper;
+
+    private final String UPLOAD_DIR = "uploads/profile-pictures/";
+
+    @Override
+    public User getCurrentLoggedInUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null) {
+            throw new NotFoundException("User is not authenticated");
+        }
+        String email = authentication.getName();
+
+        return userRepo.findByEmail(email).orElseThrow(() -> new NotFoundException("User not found"));
+    }
+
+    @Override
+    public Response<UserDTO> getMyProfile() {
+        User user = getCurrentLoggedInUser();
+        UserDTO userDTO = modelMapper.map(user, UserDTO.class);
+
+        return Response.<UserDTO>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("User retrieved")
+                .data(userDTO)
+                .build();
+    }
+
+    @Override
+    public Response<Page<UserDTO>> getAllUsers(int page, int size) {
+        Page<User> users = userRepo.findAll(PageRequest.of(page, size));
+
+        Page<UserDTO> userDTOS = users.map(user -> modelMapper.map(user, UserDTO.class));
+
+        return Response.<Page<UserDTO>>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Users retrieved")
+                .data(userDTOS)
+                .build();
+    }
+
+    @Override
+    public Response<?> updatePassword(UpdatePasswordRequest updatePasswordRequest) {
+        User user = getCurrentLoggedInUser();
+
+        String newPassword = updatePasswordRequest.getNewPassword();
+        String oldPassword = updatePasswordRequest.getOldPassword();
+
+        if (oldPassword == null || newPassword == null) {
+            throw new BadRequestException("Old and New Password Required");
+        }
+
+        // Validate the old password.
+        if (!passwordEncoder.matches(oldPassword, user.getPassword())) {
+            throw new BadRequestException("Old Password not Correct");
+        }
+        user.setPassword(passwordEncoder.encode(newPassword));
+        user.setUpdatedAt(LocalDateTime.now());
+
+        userRepo.save(user);
+
+        // Send password change confirmation email.
+        Map<String, Object> templateVariables = new HashMap<>();
+        templateVariables.put("name", user.getFirstName());
+
+        NotificationDTO notificationDTO = NotificationDTO.builder()
+                .recipient(user.getEmail())
+                .subject("Your Password Was Successfully Changed")
+                .templateName("password-change")
+                .templateVariables(templateVariables)
+                .build();
+
+        notificationService.sendEmail(notificationDTO, user);
+
+        return Response.builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Password Changed Successfully")
+                .build();
+    }
+
+    @Override
+    public Response<?> uploadProfilePicture(MultipartFile file) {
+        User user = getCurrentLoggedInUser();
+
+        try {
+            Path uploadPath = Paths.get(UPLOAD_DIR);
+
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectories(uploadPath);
+            }
+
+            if (user.getProfilePictureUrl() != null && !user.getProfilePictureUrl().isEmpty()) {
+                Path oldFile = Paths.get(user.getProfilePictureUrl());
+                if (Files.exists(oldFile)) {
+                    Files.delete(oldFile);
+                }
+            }
+
+            // Generate a unique file name to avoid conflicts
+            String originalFileName = file.getOriginalFilename();
+            String fileExtension = "";
+            if (originalFileName != null && originalFileName.contains(".")) {
+                fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
+            }
+
+            String newFileName = UUID.randomUUID() + fileExtension;
+            Path filePath = uploadPath.resolve(newFileName);
+
+            Files.copy(file.getInputStream(), filePath);
+
+            String fileUrl = UPLOAD_DIR + newFileName;
+
+
+            user.setProfilePictureUrl(fileUrl);
+            userRepo.save(user);
+
+            return Response.builder()
+                    .statusCode(HttpStatus.OK.value())
+                    .message("Profile picture uploaded successfully.")
+                    .data(fileUrl)
+                    .build();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+}
