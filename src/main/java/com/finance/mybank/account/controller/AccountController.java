35763573fe66c9293709c691b74a302a79066c90
Marcin <marcinbielen995@gmail.com>
2026-08-24 10:22:17 +0200

    Create AccountController

---
+++ src/main/java/com/finance/mybank/account/controller/AccountController.java
@@ -0,0 +1,26 @@
+package com.finance.mybank.account.controller;
+
+import com.finance.mybank.account.service.AccountService;
+import com.finance.mybank.res.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @GetMapping("/me")
+    public ResponseEntity<Response<?>> getMyAccounts() {
+        return ResponseEntity.ok(accountService.getMyAccounts());
+    }
+
+    @DeleteMapping("/close/{accountNumber}")
+    public ResponseEntity<Response<?>> closeAccount(@PathVariable String accountNumber) {
+        return ResponseEntity.ok(accountService.closeAccount(accountNumber));
+    }
+
+}
